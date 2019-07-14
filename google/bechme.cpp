652f87544961d9450371068efd451dea007edf4d
Mehmet Ali Baykara <31475871+Celcis@users.noreply.github.com>
2019-07-14 14:40:45 +0200

    Update bechme.cpp

--- google/bechme.cpp
+++ google/bechme.cpp
@@ -1,12 +1,34 @@
 #include<benchmark/benchmark.h>
 #include <vector>
 
-static void bench_(benchmark::State &state){
-	while(state.KeepRunning()){
-		std::vector<int> v;
-		v.push_back(42);
-	}
+static void bench_create(benchmark::State &state){
+        while(state.KeepRunning()){
+                std::vector<int> v;
+                (void) v;
+        }
 }
 
-BENCHMARK(bench_);
+BENCHMARK(bench_create);
+
+static void bench_reserve(benchmark::State &state){
+        while(state.KeepRunning()){
+                std::vector<int> v;
+                v.reserve(1);
+
+        }
+}
+
+BENCHMARK(bench_reserve);
+
+
+static void bench_push_back(benchmark::State &state){
+        while(state.KeepRunning()){
+                std::vector<int> v;
+                v.reserve(1);
+                v.push_back(42);
+        }
+}
+
+BENCHMARK(bench_push_back);
+                                                                                                                              1,0-1         Top
 BENCHMARK_MAIN();
